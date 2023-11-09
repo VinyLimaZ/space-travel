af634b45ce32e04d03a5363a769e31b54798beb3
Vinicius 'VinyLimaZ' Lima <vinylimaz@live.com>
2023-11-09 13:09:22 -0300

    Remove unnecessary validation in Planet

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,4 @@
 class Planet < ApplicationRecord
-  validates :name, :earth_distance, presence: true
+  validates :name, presence: true
   validates :earth_distance, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/planet_spec.rb
+++ spec/models/planet_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Planet, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:earth_distance) }
     it { should validate_numericality_of(:earth_distance).is_greater_than(0).only_integer }
   end
 end
