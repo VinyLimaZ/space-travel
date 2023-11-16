51ea2f8e29cb163bcd348dbe2c0fd9ca1a37afa4
Vinicius 'VinyLimaZ' Lima <vinylimaz@live.com>
2023-11-16 12:37:20 -0300

    Add locale to state transition errors

--- app/validators/state_transition_validator.rb
+++ app/validators/state_transition_validator.rb
@@ -1,58 +1,58 @@
 class StateTransitionValidator < ActiveModel::EachValidator
-  def validate_each(record, _status_enum, _value)
+  def validate_each(record, attribute, _value)
     return unless record.will_save_change_to_status?
 
-    validate_transition(record)
+    validate_transition(record, attribute)
   end
 
   private
 
-  def validate_transition(record)
-    initial_state(record) if record.status_was.nil?
-    blocked_transition(record) if record.status_was.in? record.final_state
-    scheduled_transition(record) if record.status_was.in? record.initial_state
-    started_transition(record) if record.status_was.in? record.middle_state
+  def validate_transition(record, attribute)
+    initial_state(record, attribute) if record.status_was.nil?
+    blocked_transition(record, attribute) if record.status_was.in? record.final_state
+    scheduled_transition(record, attribute) if record.status_was.in? record.initial_state
+    started_transition(record, attribute) if record.status_was.in? record.middle_state
   end
 
-  def initial_state(record)
+  def initial_state(record, attribute)
     return if record.scheduled?
 
-    initial_state_error_message(record)
+    initial_state_error_message(record, attribute)
   end
 
-  def initial_state_error_message(record)
-    record.errors.add :status, 'need to start with scheduled'
+  def initial_state_error_message(record, attribute)
+    record.errors.add(attribute, :wrong_start)
   end
 
-  def scheduled_transition(record)
+  def scheduled_transition(record, attribute)
     return if record.errors.any?
     return if record.middle_state?
 
-    scheduled_transition_error(record)
+    scheduled_transition_error(record, attribute)
   end
 
-  def scheduled_transition_error(record)
-    record.errors.add :status, 'need to transit to started or aborted'
+  def scheduled_transition_error(record, attribute)
+    record.errors.add(attribute, :wrong_middle_transition)
   end
 
-  def started_transition(record)
+  def started_transition(record, attribute)
     return if record.errors.any?
     return if record.final_state?
 
-    started_transition_error(record)
+    started_transition_error(record, attribute)
   end
 
-  def started_transition_error(record)
-    record.errors.add :status, 'need to transit to aborted, failed or finished'
+  def started_transition_error(record, attribute)
+    record.errors.add(attribute, :wrong_final_transition)
   end
 
-  def blocked_transition(record)
+  def blocked_transition(record, attribute)
     return if record.errors.any?
 
-    blocked_transition_error(record)
+    blocked_transition_error(record, attribute)
   end
 
-  def blocked_transition_error(record)
-    record.errors.add :status, 'is in the final state and can\'t be changed! Please create another trip'
+  def blocked_transition_error(record, attribute)
+    record.errors.add(attribute, :blocked_to_change)
   end
 end
